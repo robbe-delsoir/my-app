ace628ffa9826fec5ca5906d654d44f3154cecf2
robbe.delsoir <robbe.delsoir@student.vives.be>
2021-12-08 00:01:29 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ApiInfo from './components/ApiInfo'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <div>
+  <body>
+    <div className="header">
+      <Header/>
     </div>
+    <div class="api">
+    <ApiInfo/>
+    </div>
+    <div className="footer">
+      <Footer/>
+    </div>
+    </body>
+  </div>
   );
+
+  
 }
 
+
 export default App;
+
